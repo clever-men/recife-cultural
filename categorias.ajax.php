6c2e75daa3aa7ef226f75d8d0b871d681215758c
lucascardozo <lucas_lrrc@hotmail.com>
2016-07-02 18:15:36 -0300

    Versão 1

---
+++ categorias.ajax.php
@@ -0,0 +1,82 @@
+<?php
+	header( 'Cache-Control: no-cache' );
+	header( 'Content-type: application/xml; charset="utf-8"', true );
+
+	$cod_categorias = mysql_real_escape_string( $_REQUEST['cod_categorias'] );
+
+	$categorias = array();
+	
+	if($cod_categorias == 1){
+	
+		//Setando o valor dos arrays
+		
+		$categoria = array('kinopex','Cinemark','Cinepolis');
+		
+		//Agora montar o foreach
+	
+		foreach ($categoria as $key=>$value) {
+		
+			 $categorias[] = array(
+					'cod_local'	=> $key,
+					'nome'			=> $value,
+			);
+		}
+	
+	}else if($cod_categorias == 2){
+	
+	//Setando o valor dos arrays
+		
+		$categoria = array('Boa Viagem','Pina','Piedade');
+		
+		//Agora montar o foreach
+	
+		foreach ($categoria as $key=>$value) {
+		
+			 $categorias[] = array(
+					'cod_local'	=> $key,
+					'nome'			=> $value,
+			);
+		}
+	
+		
+	
+	}else if($cod_categorias == 3){
+	
+	//Setando o valor dos arrays
+		
+		$categoria = array('Museo Brenand','Museo Da cidade','Museo da abolicao');
+		
+		//Agora montar o foreach
+	
+		foreach ($categoria as $key=>$value) {
+		
+			 $categorias[] = array(
+					'cod_local'	=> $key,
+					'nome'			=> $value,
+			);
+		}
+	
+		
+	
+	}else if($cod_categorias == 4){
+	
+	//Setando o valor dos arrays
+		
+		$categoria = array('Shopping Rio Mar','Shopping Plaza','Shopping Recife');
+		
+		//Agora montar o foreach
+	
+		foreach ($categoria as $key=>$value) {
+		
+			 $categorias[] = array(
+					'cod_local'	=> $key,
+					'nome'			=> $value,
+			);
+		}
+	
+		
+	
+	}
+	
+
+	echo( json_encode( $categorias ) );
---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Recife Cultural">
+    <meta name="author" content="Recife Cultural">
+    <link rel="icon" href="img/favicon.ico">
+
+    <title>Recife Cultural</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="header clearfix">
+		<br>
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">Sobre</a></li>
+            <li role="presentation"><a href="#">Contato</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">Recife Cultural	</h3>
+      </div>
+
+      <div class="jumbotron">
+        <h1>Escolha uma opção abaixo</h1>
+        <p class="lead">Esse site contem vários pontos turísticos para que você possa visitar na veneza Brasileira.</p>
+        <form id="form">
+			<label for="cod_categorias">Categoria:</label>
+			
+			<select name="cod_categorias" id="cod_categorias">
+				<option value="">-- Escolha uma categoria --</option>
+				<option value="1">-- CINEMA --</option>
+				<option value="2">-- PRAIAS --</option>
+				<option value="3">-- MUSEUS --</option>
+				<option value="4">-- SHOPPINGS --</option>
+			</select>
+		
+		</form>
+      </div>
+	  <div class="jumbotron" >
+		<ul id="lista" ></ul>
+	  </div>
+
+      <div class="row marketing">
+        <div class="col-lg-6">
+         
+        </div>
+
+        <div class="col-lg-6">
+          
+        </div>
+      </div>
+
+      <footer class="footer">
+        <p>&copy; 2016 Company, Inc.</p>
+      </footer>
+
+    </div> <!-- /container -->
+	<script src="http://www.google.com/jsapi"></script>
+	<script type="text/javascript">
+		 google.load('jquery', '1.3');
+	</script>		
+
+	<script type="text/javascript">
+		$(function(){
+			$('#cod_categorias').change(function(){
+				if( $(this).val() ) {
+					$('#lista').hide();
+					$('.carregando').show();
+					$.getJSON('categorias.ajax.php?search=',{cod_categorias: $(this).val(), ajax: 'true'}, function(j){
+						var options = '';	
+						for (var i = 0; i < j.length; i++) {
+							options += '<li>' + j[i].nome + '</li>';
+						}	
+						$('#lista').html(options).show();
+						$('.carregando').hide();
+					});
+				} else {
+					$('#lista').html('<li>– Nenhum item encontrado –</li>');
+				}
+			});
+		});
+	</script>
+  </body>
+</html>
